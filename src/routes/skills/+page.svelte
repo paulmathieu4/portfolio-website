de56191aabecd1307c93c801e9f4e38e8877c158
paul MATHIEU <paul.mathieu4@gmail.com>
2025-10-14 21:10:49 +0200

    feat: enhanced skills radar with multiple datasets, updated tsconfig, and expanded skills data

--- src/routes/skills/+page.svelte
+++ src/routes/skills/+page.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-    import { Radar } from 'svelte-chartjs';
+    import {Radar} from 'svelte-chartjs';
 
     import {
         Chart as ChartJS,
@@ -9,6 +9,7 @@
         PointElement,
         RadialLinearScale,
         LineElement,
+        Filler,
     } from 'chart.js';
     import {type Skill, skills} from './skills.data';
 
@@ -18,38 +19,104 @@
         Legend,
         PointElement,
         RadialLinearScale,
-        LineElement
+        LineElement,
+        Filler,
     );
+
+    let frontendData: (number | null)[] = Array.from({length: skills.length}, () => null);
+    let backendData: (number | null)[] = [...frontendData];
+    let databaseData: (number | null)[] = [...frontendData];
+    let generalTechData: (number | null)[] = [...frontendData];
+
+    initializeTechnicalSkillsChartData();
 
     // Data for the line chart
     const data = {
         labels: skills.map((skill: Skill) => skill.name),
         datasets: [
             {
-                label: 'Compétences techniques',
-                backgroundColor: '#ec4899',
+                label: 'Front-end',
                 borderColor: '#ec4899',
-                data: skills.map((skill: Skill) => skill.level),
+                backgroundColor: 'rgba(236,72,153,0.4)',
+                data: frontendData,
                 fill: true,
-            }
+            },
+            {
+                label: 'Back-end',
+                borderColor: '#32387B',
+                backgroundColor: 'rgba(50,56,123,0.4)',
+                data: backendData,
+                fill: true,
+            }, {
+                label: 'Databases',
+                borderColor: '#327b41',
+                backgroundColor: 'rgba(50,123,65,0.4)',
+                data: databaseData,
+                fill: true,
+            }, {
+                label: 'General',
+                borderColor: '#7b3232',
+                backgroundColor: 'rgba(123,50,50,0.4)',
+                data: generalTechData,
+                fill: true,
+            },
         ],
     };
 
+    const radarChartOptions = {
+        maintainAspectRatio: false,
+        responsive: true,
+        scales: {
+            r: {
+                suggestedMin: 1,
+                suggestedMax: 10
+            }
+        },
+        plugins: {
+            title: {
+                text: 'Technical Skills',
+                display: true,
+            }
+        }
+    };
+
+    function initializeTechnicalSkillsChartData() {
+        frontendData = Array.from({length: skills.length}, () => null);
+        backendData = [...frontendData];
+        databaseData = [...frontendData];
+        generalTechData = [...frontendData];
+
+        for (let i = 0; i < skills.length; i++) {
+            const currentSkill = skills[i];
+            if (currentSkill.areas.includes('frontend')) {
+                frontendData[i] = currentSkill.level;
+            }
+            if (currentSkill.areas.includes('backend')) {
+                backendData[i] = currentSkill.level;
+            }
+            if (currentSkill.areas.includes('database')) {
+                databaseData[i] = currentSkill.level;
+            }
+            if (currentSkill.areas.includes('general')) {
+                generalTechData[i] = currentSkill.level;
+            }
+        }
+    }
+
 </script>
 
-<div class="w-full max-w-2xl mx-auto p-4">
-    <Radar {data} options={{ responsive: true, scales: {
-        r: {
-            suggestedMin: 1,
-            suggestedMax: 10
-        }
-    } }} />
+<div class="preset-content-width">
+    <Radar {data}
+           options={radarChartOptions}
+           width={400}
+           height={660}
+    />
 </div>
 
 <style>
     div {
         background: white;
         border-radius: 1rem;
-        box-shadow: 0 2px 8px rgba(0,0,0,0.1);
+        box-shadow: 0 2px 8px rgba(0, 0, 0, 0.1);
     }
 </style>
--- src/routes/skills/skills.data.ts
+++ src/routes/skills/skills.data.ts
@@ -4,7 +4,7 @@ export interface Skill {
 	areas: SkillArea[];
 };
 
-export type SkillArea = "frontend" | "backend" | "technical";
+export type SkillArea = "frontend" | "backend" | "technical" | "general" | "database" ;
 
 export const skills: Skill[] = [
 	{ name: "JavaScript / Typescript", level: 9, areas: ["frontend", "technical"] },
@@ -14,4 +14,19 @@ export const skills: Skill[] = [
 	{ name: "React", level: 6, areas: ["frontend", "technical"] },
 	{ name: ["CSS Design Systems", "(Tailwind, Bootstrap, Materialize)"], level: 9, areas: ["frontend", "technical"] },
 	{ name: "Responsive / Adaptive Design", level: 9, areas: ["frontend", "technical"] },
+	{ name: "Node.js (NestJS, Express)", level: 9, areas: ["backend", "technical"] },
+	{ name: "Java (SprintBoot, JEE, Grails, Android)", level: 8, areas: ["backend", "technical"] },
+	{ name: "REST API", level: 9, areas: ["backend", "technical"] },
+	{ name: "C# (ASP.NET)", level: 4, areas: ["backend", "technical"] },
+	{ name: "SQL (MySQL, PostgreSQL, MSSQL)", level: 9, areas: ["database", "technical"] },
+	{ name: "NoSQL (MongoDB, CouchDB)", level: 7, areas: ["database", "technical"] },
+	{ name: "LDAP Directories (OpenLDAP, Microsoft Active Directory)", level: 6, areas: ["database", "technical"] },
+	{ name: "Software Architecture", level: 9, areas: ["general", "technical"] },
+	{ name: "Scripting (Bash, PowerShell, Perl)", level: 5, areas: ["general", "technical"] },
+	{ name: "Testing (unit tests, end-to-end)", level: 9, areas: ["general", "technical"] },
+	{ name: "Git", level: 9, areas: ["general", "technical"] },
+	{ name: "Interactive Map Libraries (MapBox/MapLibre)", level: 8, areas: ["general", "technical"] },
+	{ name: "Design (Photoshop, Figma, Gimp)", level: 5, areas: ["general", "technical"] },
+	{ name: "Docker", level: 7, areas: ["general", "technical"] },
+	{ name: "Application Servers (Tomcat, Firebase, Nginx, WebLogic, IIS)", level: 7, areas: ["general", "technical"] },
 ];
